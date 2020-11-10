66484478679a3271c94aef5479aef56003bd42f9
James <james.jasenia@gmail.com>
2020-11-11 09:22:00 +1100

    Initial Commit

---
+++ JSON-MVVM-IOSTests/JSON_MVVM_IOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  JSON_MVVM_IOSTests.swift
+//  JSON-MVVM-IOSTests
+//
+//  Created by James Jasenia on 11/11/20.
+//
+
+import XCTest
+@testable import JSON_MVVM_IOS
+
+class JSON_MVVM_IOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
